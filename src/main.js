dfdbd0cc3fa925e4d19dd2635cecfd1a1e09d590
Abhishek <abhishek.kv@vandalayglobal.com>
2024-11-16 13:57:29 +0530

    Dynamic content added: Added dynamic contents and also added styles to footer contact link.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import "./css/styles.css";
-import datas from "./js/data.json";
+import overviewContent from "./js/overviewContent.json";
+import previewContent from "./js/previewContent.json";
 /*!
  * Start Bootstrap - Freelancer v7.0.7 (https://startbootstrap.com/theme/freelancer)
  * Copyright 2013-2023 Start Bootstrap
@@ -54,69 +55,131 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
 /*  TODO: Dynamic upadation and  handle Dynamic
 data alng with Onclick handle for popup*/
+// over view modal  popup code
+// function modalContents() {
+//   console.log("Btn clicked");
+//   const portfolioOverview = document.getElementById(
+//     "portfolio-content-overview",
+//   );
+//   const portfolioOverviewTemplate = document.getElementById(
+//     "content-overview-template",
+//   );
+//   const overviewImageTemplate = document.getElementById(
+//     "overview-image-carousel",
+//   );
+//   let portpolioContent,
+//     images,
+//     contentHeading,
+//     contentDescription,
+//     carouselImages;
 
-const portfolioContainer = document.getElementById("portfolio-container");
-const protfolioContentTemplate = document.getElementById(
-  "portfolio-item-template",
-);
-let content2, content3, content;
-content = protfolioContentTemplate.content.cloneNode(true);
-content2 = protfolioContentTemplate.content.cloneNode(true);
-content3 = protfolioContentTemplate.content.cloneNode(true);
+//   portpolioContent = portfolioOverviewTemplate.content.cloneNode(true);
+//   // carouselImages = overviewImageTemplate.content.cloneNode(true);
 
-portfolioContainer.appendChild(content);
-portfolioContainer.appendChild(content2);
-portfolioContainer.appendChild(content3);
+//   images = portpolioContent.querySelector("#content-overview-images");
+//   contentHeading = portpolioContent.querySelector("#content-overview-title");
+//   contentDescription = portpolioContent.querySelector(
+//     "#content-overview-discription",
+//   );
+//   images.innerHTML = "";
+//   overviewContent.forEach((data) => {
+//     contentHeading.innerHTML = data.title;
+//     contentDescription.innerHTML = data.discription;
+//     data?.imageContents.forEach((imageSrc, index) => {
+//       let carouselImageContainer =
+//         overviewImageTemplate.content.cloneNode(true);
+//       let carouselImage = carouselImageContainer.querySelector("img");
+//       let carouselContainer =
+//         carouselImageContainer.querySelector(".carousel-item");
+//       carouselImage.setAttribute("src", imageSrc);
+//       carouselImage.setAttribute(
+//         "alt",
+//         imageSrc.split("/").pop().split(".").slice(0, -1).join("."),
+//       );
+//       if (0 === index) {
+//         carouselContainer.classList.add("active");
+//       }
 
-function modalContents() {
-  console.log("Btn clicked");
-  const portfolioOverview = document.getElementById(
-    "portfolio-content-overview",
+//       images.appendChild(carouselImageContainer);
+//     });
+//   });
+//   console.log("iamges", images);
+//   portfolioOverview.appendChild(portpolioContent);
+// }
+
+//dynamic content  code
+function previewContentShow() {
+  const portfolioContainer = document.getElementById("portfolio-container");
+  const protfolioContentTemplate = document.getElementById(
+    "portfolio-item-template",
   );
-  const portfolioOverviewTemplate = document.getElementById(
-    "content-overview-template",
-  );
-  const overviewImageTemplate = document.getElementById(
-    "overview-image-carousel",
-  );
-  let portpolioContent,
-    images,
-    contentHeading,
-    contentDescription,
-    carouselImages;
-
-  portpolioContent = portfolioOverviewTemplate.content.cloneNode(true);
-  // carouselImages = overviewImageTemplate.content.cloneNode(true);
-
-  images = portpolioContent.querySelector("#content-overview-images");
-  contentHeading = portpolioContent.querySelector("#content-overview-title");
-  contentDescription = portpolioContent.querySelector(
-    "#content-overview-discription",
-  );
-  images.innerHTML = "";
-  datas.forEach((data) => {
-    contentHeading.innerHTML = data.title;
-    contentDescription.innerHTML = data.discription;
-    data?.imageContents.forEach((imageSrc, index) => {
-      let carouselImageContainer =
-        overviewImageTemplate.content.cloneNode(true);
-      let carouselImage = carouselImageContainer.querySelector("img");
-      let carouselContainer =
-        carouselImageContainer.querySelector(".carousel-item");
-      carouselImage.setAttribute("src", imageSrc);
-      carouselImage.setAttribute(
-        "alt",
-        imageSrc.split("/").pop().split(".").slice(0, -1).join("."),
+  // let content2, content3, content;
+  // content2 = protfolioContentTemplate.content.cloneNode(true);
+  // content3 = protfolioContentTemplate.content.cloneNode(true);
+  previewContent.forEach((preview) => {
+    console.log("preview", previewContent, preview);
+    let content = protfolioContentTemplate.content.cloneNode(true);
+    let previewContentShow = content.querySelector("#content-preview");
+    // const overviewClick = document.getElementById("overview-click");
+    // overviewClick.addEventListener("click", modalContents);
+    if (preview.content.media.video) {
+      previewContentShow.appendChild(videoContentPrview(preview.content.media));
+    }
+    if ("" === preview.content.media.video && preview.content.media.image) {
+      previewContentShow.appendChild(
+        imageContentPreview(preview.content.media.image),
       );
-      if (0 === index) {
-        carouselContainer.classList.add("active");
-      }
+    }
 
-      images.appendChild(carouselImageContainer);
-    });
+    portfolioContainer.appendChild(content);
   });
-  console.log("iamges", images);
-  portfolioOverview.appendChild(portpolioContent);
+
+  // portfolioContainer.appendChild(content2);
+  // portfolioContainer.appendChild(content3);
+  // type="video/mp4">
+  //   <source src="video.webm" type="video/webm">
+  //   <source src="video.ogg" type="video/ogg">
+  //
 }
-const overviewClick = document.getElementById("overview-click");
-overviewClick.addEventListener("click", modalContents);
+function videoContentPrview(src) {
+  const video = document.createElement("video");
+  video.autoplay = true;
+  video.muted = true;
+  video.loop = true;
+  video.classList.add("img-fluid");
+  const type = src.video.substring(src.video.lastIndexOf(".") + 1);
+  let source = document.createElement("source");
+  switch (type) {
+    case "mp4":
+      source.type = "video/mp4";
+      source.src = src.video;
+      break;
+    case "ogg":
+      source.type = "video/ogg";
+      source.src = src.video;
+      break;
+    case "webm":
+      source.type = "video/webm";
+      source.src = src.video;
+      break;
+    default:
+      source.type = "";
+      source.src = "";
+      break;
+  }
+  video.appendChild(source);
+  video.onerror = () => {
+    console.log("onerror", src.image);
+    video.style.display = "none";
+    imageContentPreview(src.image);
+  };
+  return video;
+}
+function imageContentPreview(src) {
+  const img = document.createElement("img");
+  img.src = src;
+  img.alt = src.split("/").pop().split(".").slice(0, -1).join(".");
+  return img;
+}
+
+previewContentShow();
