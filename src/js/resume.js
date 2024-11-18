60a13bada963e04f666acfc03244567a3881c9ed
Abhishek <abhishek.kv@vandalayglobal.com>
2024-11-18 14:09:51 +0530

    Started for handling pdf resume genaration: Today started pdf genration process for resume but has got problems in pdf optimaization. Pdf not looking good and styles  need reconfiguring.
    * Fonts need to handled.=> responsive font-size,line-height,content-size.
    * image portfolio handled.=> iphone doesnot take out-line property.
    * prefer sematics html.
    * responsive design handle

--- src/js/resume.js
+++ src/js/resume.js
@@ -1,5 +1,35 @@
 import "../css/styles.css";
 import "../css/resume-style.css";
 import profileResume from "../assets/img/profile_pic.jpg";
+import jsPDF from "jspdf";
+import html2pdf from "html2pdf.js";
+import html2canvas from "html2canvas";
 
-document.getElementById("profile-resume").src = profileResume;
+const profile = document.getElementById("profile-resume");
+profile.alt = "profile";
+profile.src = profileResume;
+window.addEventListener("load", (event) => {
+  console.log("pdf genrate");
+  const options = {
+    margin: 0,
+    filename: "Amaresh-Muraleedharan-resume.pdf", //name the output file
+    image: {
+      type: "jpeg", //image type
+      quality: 100,
+    },
+    html2canvas: {
+      scale: 1.75,
+    },
+    jsPDF: {
+      unit: "px",
+      format: "a4",
+      orientation: "portrait", // pdf orientation
+    },
+  };
+
+  // $('.btn-download').click(function(e){     // class for download button
+  //   e.preventDefault();
+  const element = document.getElementById("resume"); //id for content area
+  // html2pdf().from(element).set(options).save();
+  // });
+});
